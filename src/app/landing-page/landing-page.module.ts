be8ba876170f41feb106e5cfe4580f5d1018ab86
Jairo Cesar Villadiego Contreras <jcvilladiegoc@gmail.com>
2023-03-22 12:45:01 -0500

    #3 #comment The nav bar was completed

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule,
     LandingPageRoutingModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class LandingPageModule { }
