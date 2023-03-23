77538af4786575bd21d2c892eb53b1240db007f3
Jairo Cesar Villadiego Contreras <jcvilladiegoc@gmail.com>
2023-03-22 22:09:09 -0500

    #3 #comment The available rooms section was completed

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -5,6 +5,8 @@ import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RoomCardModule } from '../shared/room-card/room-card.module';
+import { MatPaginatorModule } from '@angular/material/paginator'
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     LandingPageRoutingModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    RoomCardModule,
+    MatPaginatorModule
   ]
 })
 export class LandingPageModule { }
---
+++ src/app/shared/room-card/room-card.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoomCardComponent } from './room-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    RoomCardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    RoomCardComponent
+  ]
+})
+export class RoomCardModule { }
